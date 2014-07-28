c0c9564c212d46c3e9871de5951b782bc8df94d5
spdavern <sean@lizandsean.com>
2014-07-28 16:46:13 -0700

    Added button functionality with routings and button ng-hides.

--- app/cnc-app.js
+++ app/cnc-app.js
@@ -1,9 +1,13 @@
-var myApp = angular.module('cncApp', ["ui.router", "ngAnimate"]);
-myApp.constant('VERSION', "0.1");
-myApp.run(function(VERSION, $rootScope){
-	 	$rootScope.version = VERSION;
-	 });
-myApp.config(function($stateProvider, $urlRouterProvider) {
+var myApp = angular.module('cncApp', ['ui.router', 'ngAnimate']);
+myApp.constant('VERSION', "0.2");
+myApp.run( ['$rootScope', 'cncData', '$state', '$stateParams',
+			function($rootScope, cncData, $state, $stateParams) {
+				$rootScope.data = cncData;
+				$rootScope.$state = $state;
+				$rootScope.$stateParams = $stateParams;
+}]);
+myApp.config( ['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
 	//
 	// For any unmatched url, redirect to /
 	$urlRouterProvider.otherwise("/");
@@ -12,6 +16,16 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider
 		.state("home", {
 			url: "/",
-			templateUrl: "partials/home/home.html"
+			templateUrl: "partials/home/home.html",
 		})
-});
+		.state("countries", {
+			url: "/countries",
+			templateUrl: "partials/countries/countries.html",
+		})
+}]);
+myApp.factory('cncData', ['VERSION',
+	function(VERSION) {
+		var Data = {};
+	 	Data.version = VERSION;
+		return Data		
+	}]);
